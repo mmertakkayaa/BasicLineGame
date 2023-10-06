7c563268824b6319f4fc9de916d478ee2237f68a
Mert Akkaya <akkayamert34@gmail.com>
2023-10-06 15:51:21 +0300

    commit added

---
+++ main.py
@@ -0,0 +1,40 @@
+import turtle
+
+drawing_board = turtle.Screen()
+drawing_board.bgcolor("light blue")
+drawing_board.title("Phyton Turtle")
+
+turtle_instance = turtle.Turtle()
+
+
+def turtle_forward():
+    turtle_instance.forward(100)
+
+def rotate_angle_right():
+    turtle_instance.right(90)
+
+def rotate_angle_left():
+    turtle_instance.left((90))
+
+def clear_screen():
+    turtle_instance.clear()
+
+def turtle_return_home():
+    turtle_instance.penup()
+    turtle_instance.home()
+    turtle_instance.pendown()
+def turtle_pen_up():
+    turtle_instance.penup()
+
+def turtle_pen_down():
+    turtle_instance.pendown()
+
+drawing_board.listen()
+drawing_board.onkey(fun=turtle_forward,key="space")
+drawing_board.onkey(fun=rotate_angle_right,key="Down")
+drawing_board.onkey(fun=rotate_angle_left,key="Up")
+drawing_board.onkey(fun=clear_screen, key="c")
+drawing_board.onkey(fun=turtle_return_home, key="h")
+drawing_board.onkey(fun=turtle_pen_up, key="1")
+drawing_board.onkey(fun=turtle_pen_down, key="2")
+turtle.mainloop()
